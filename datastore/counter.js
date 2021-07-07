55571a1fb54b1929027294350fa54efe499ab556
Shruti Agrawal <shruti98932@gmail.com>
2021-07-07 14:02:13 -0400

    Implemented All CRUD functions

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,15 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, counter) => {
+    if (err) {
+      throw ('error occured');
+    } else {
+      writeCounter(counter + 1, callback);
+    }
+  });
+
 };
 
 
--- datastore/index.js
+++ datastore/index.js
@@ -3,44 +3,114 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
+
 var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  var id = counter.getNextUniqueId((err, id) => {
+    if (err) {
+      throw ('Cannot create a todo with id');
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
+
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, files.map(eachFile => {
+        return {
+          id: path.basename(eachFile, '.txt'),
+          text: path.basename(eachFile, '.txt')
+        };
+      }));
+    }
+  });
+
+};
+/*var data = _.map(items, (text, id) => {
     return { id, text };
   });
   callback(null, data);
-};
-
+  */
 exports.readOne = (id, callback) => {
-  var text = items[id];
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err, file) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, {id: id, text: file.toString()});
+    }
+  });
+};
+/*
+ var text = items[id];
   if (!text) {
     callback(new Error(`No item with id: ${id}`));
   } else {
     callback(null, { id, text });
   }
+  */
+exports.update = (id, text, callback) => {
+  fs.access(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          throw (`No item with id: ${id}`);
+        } else {
+          callback(null, id);
+        }
+      });
+    }
+  });
 };
 
-exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
-};
+
+/*fs.open(`${exports.dataDir}/${id}.txt`, 'r+', (err, fd) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.write(fd, text, (err, written, string) => {
+        if (err) {
+          throw ('Error while updating');
+        } else {
+          fs.close(fd, (err) => {
+            if (err) {
+              throw ('error closing the file');
+            } else {
+              callback(null, id);
+            }
+          });
+        }
+      });
+    }
+  });
+};*/
 
 exports.delete = (id, callback) => {
-  var item = items[id];
+  fs.rm(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(new Error(`No item to remove with id: ${id}`));
+    } else {
+      callback(null, id);
+    }
+  });
+};
+
+/* var item = items[id];
   delete items[id];
   if (!item) {
     // report an error if item not found
@@ -48,8 +118,7 @@ exports.delete = (id, callback) => {
   } else {
     callback();
   }
-};
-
+  */
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
 exports.dataDir = path.join(__dirname, 'data');
